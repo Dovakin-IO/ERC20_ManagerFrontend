f371c6b538162ea09ebbef9cf7913df813d9c456
Dovakin <dovakin@DovakindeMac-mini.local>
2018-11-05 14:34:24 +0800

    修复若干bug

---
+++ src/pages/.umi/initDva.js
@@ -0,0 +1,106 @@
+import dva from "dva";
+import createLoading from "dva-loading";
+
+const runtimeDva = window.g_plugins.mergeConfig("dva");
+let app = dva({
+  history: window.g_history,
+
+  ...(runtimeDva.config || {})
+});
+
+window.g_app = app;
+app.use(createLoading());
+(runtimeDva.plugins || []).forEach(plugin => {
+  app.use(plugin);
+});
+
+app.model({
+  namespace: "global",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/models/global.js")
+    .default
+});
+app.model({
+  namespace: "list",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/models/list.js")
+    .default
+});
+app.model({
+  namespace: "login",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/models/login.js")
+    .default
+});
+app.model({
+  namespace: "project",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/models/project.js")
+    .default
+});
+app.model({
+  namespace: "setting",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/models/setting.js")
+    .default
+});
+app.model({
+  namespace: "user",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/models/user.js")
+    .default
+});
+app.model({
+  namespace: "register",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/User/models/register.js")
+    .default
+});
+app.model({
+  namespace: "activities",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/Dashboard/models/activities.js")
+    .default
+});
+app.model({
+  namespace: "chart",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/Dashboard/models/chart.js")
+    .default
+});
+app.model({
+  namespace: "monitor",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/Dashboard/models/monitor.js")
+    .default
+});
+app.model({
+  namespace: "form",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/Forms/models/form.js")
+    .default
+});
+app.model({
+  namespace: "tx",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/TxList/models/tx.js")
+    .default
+});
+app.model({
+  namespace: "txdetail",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/TxList/models/txdetail.js")
+    .default
+});
+app.model({
+  namespace: "txuser",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/TxUser/models/txuser.js")
+    .default
+});
+app.model({
+  namespace: "rule",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/List/models/rule.js")
+    .default
+});
+app.model({
+  namespace: "profile",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/Profile/models/profile.js")
+    .default
+});
+app.model({
+  namespace: "error",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/Exception/models/error.js")
+    .default
+});
+app.model({
+  namespace: "geographic",
+  ...require("/Users/dovakin/Documents/github/ERC20_ManagerFrontend/src/pages/Account/Settings/models/geographic.js")
+    .default
+});
--- src/pages/TxList/TxDetail.js
+++ src/pages/TxList/TxDetail.js
@@ -1,80 +1,115 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'dva';
-import DescriptionList from '@/components/DescriptionList';
-import PageHeaderWrapper from '@/components/PageHeaderWrapper';
-import styles from './TxDetail.less';
-import { routerRedux } from 'dva/router';
-import { 
-    Form,
-    Card, 
-    Timeline,
-    Divider,
-} from 'antd';
+import React, { PureComponent } from "react";
+import { connect } from "dva";
+import DescriptionList from "@/components/DescriptionList";
+import PageHeaderWrapper from "@/components/PageHeaderWrapper";
+import styles from "./TxDetail.less";
+import { routerRedux } from "dva/router";
+import moment from "moment";
+import { Form, Card, Timeline, Divider } from "antd";
 
 const { Description } = DescriptionList;
 
-@connect(({txdetail, loading}) => ({
-    txdetail,
-    loading: loading.models.txdetail,
+@connect(({ txdetail, loading }) => ({
+  txdetail,
+  loading: loading.models.txdetail
 }))
 @Form.create()
 class TxDetail extends PureComponent {
+  state = {};
 
-    state = {
+  dateFormat = datestr => {
+    if (datestr == null) return "";
+    let date = datestr.substring(0, 10);
+    let time = datestr.substring(11, 19);
+    return date + " " + time;
+  };
 
-    };
+  componentDidMount() {
+    const { location, dispatch } = this.props;
+    dispatch({
+      type: "txdetail/fetch",
+      payload: {
+        txHash: location.query.txHash
+      }
+    });
+  }
 
-    componentDidMount() {
-        const { location, dispatch } = this.props;
-        dispatch({
-          type: 'txdetail/fetch',
-          payload: {
-            txHash: location.query.txHash,
-          }
-        });
-    }
-
-    render () {
-        const {
-            txdetail: { userInfo, txInfo},
-            dispatch,
-        } = this.props;
-        return(
-            <PageHeaderWrapper title="转账详情">
-                <Card bordered={false}>
-                    <DescriptionList size="large" title="操作用户信息" style={{ marginBottom: 32 }}>
-                        <Description term="用户编号">{
-                            <span>
-                                <a href="javascript:;" onClick={() => {
-                                    dispatch(routerRedux.push({
-                                        pathname: '/txuser/user_detail',
-                                        query: {
-                                            settlement_account_name: userInfo.settlement_account_name,
-                                        }
-                                    }))
-                                }}>{userInfo.settlement_account_name}</a>
-                            </span>
-                        }</Description>
-                        <Description term="姓名">{userInfo.name}</Description>
-                        <Description term="手机号">{userInfo.mobile}</Description>
-                        <Description term="钱包地址">{userInfo.cpct_address}</Description>
-                    </DescriptionList>
-                    <Divider style={{ marginBottom: 32 }} />
-                    <Timeline>
-                        <Timeline.Item color="green">交易哈希: {txInfo.txHash}</Timeline.Item>
-                        <Timeline.Item color="green">区块高度: {txInfo.blockHeight}</Timeline.Item>
-                        <Timeline.Item color="green">交易日期: {txInfo.timestamp}</Timeline.Item>
-                        <Timeline.Item color="green">转出地址: {txInfo.token_from}</Timeline.Item>
-                        <Timeline.Item color="green">转入地址: {txInfo.token_to}</Timeline.Item>
-                        <Timeline.Item color="green">交易数量: {txInfo.value}</Timeline.Item>
-                        <Timeline.Item color="green">gas limit: {txInfo.gasLimit}</Timeline.Item>
-                        <Timeline.Item color="green">gas price: {txInfo.gasPrice}</Timeline.Item>
-                        <Timeline.Item color="green">gas used: {txInfo.gasUsed}</Timeline.Item>
-                    </Timeline>
-                </Card>
-            </PageHeaderWrapper>
-        );
-    }
+  render() {
+    const {
+      txdetail: { userInfo, txInfo },
+      dispatch
+    } = this.props;
+    return (
+      <PageHeaderWrapper title="转账详情">
+        <Card bordered={false}>
+          <DescriptionList
+            size="large"
+            title="操作用户信息"
+            style={{ marginBottom: 32 }}
+          >
+            <Description term="用户编号">
+              {
+                <span>
+                  <a
+                    href="javascript:;"
+                    onClick={() => {
+                      dispatch(
+                        routerRedux.push({
+                          pathname: "/txuser/user_detail",
+                          query: {
+                            settlement_account_name:
+                              userInfo.settlement_account_name
+                          }
+                        })
+                      );
+                    }}
+                  >
+                    {userInfo.settlement_account_name}
+                  </a>
+                </span>
+              }
+            </Description>
+            <Description term="姓名">{userInfo.name}</Description>
+            <Description term="手机号">{userInfo.mobile}</Description>
+            <Description term="钱包地址">{userInfo.cpct_address}</Description>
+          </DescriptionList>
+          <Divider style={{ marginBottom: 32 }} />
+          <Timeline>
+            <Timeline.Item color="green">
+              交易哈希: {txInfo.txHash}
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              区块高度: {txInfo.blockHeight}
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              交易日期:{" "}
+              {moment(new Date(txInfo.timestamp * 1000)).format(
+                "YYYY-MM-DD HH:mm:SS"
+              )}
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              转出地址: {txInfo.token_from}
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              转入地址: {txInfo.token_to}
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              交易数量: {txInfo.value.substring(0, txInfo.value.length - 18)}
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              gas limit: {txInfo.gasLimit + " wei"}
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              gas price: {txInfo.gasPrice + " wei"}
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              gas used: {txInfo.gasUsed + " wei"}
+            </Timeline.Item>
+          </Timeline>
+        </Card>
+      </PageHeaderWrapper>
+    );
+  }
 }
 
 export default TxDetail;
